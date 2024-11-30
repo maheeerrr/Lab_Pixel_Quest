3e17f193ec8f579873935d3a2b8a268b46faa423
maheeerrr <maheero2789@nycstudents.net>
2024-11-30 16:46:29 -0500

    commit 2

--- Assets/Scripts/PlayerJumping.cs
+++ Assets/Scripts/PlayerJumping.cs
@@ -10,6 +10,8 @@ public class PlayerJumping : MonoBehaviour
     public Transform ground;     // Where the collision will be checked for 
     public LayerMask groundMask; // The layer we're looking for 
     private bool waterCheck;     // Is the player touching water 
+    private int jumpCount = 0;   // Tracks how many jumps have been performed
+    private int maxJumps = 2;    // Max number of jumps allowed (double jump)
 
     // Start is called before the first frame update
     void Start()
@@ -20,28 +22,33 @@ public class PlayerJumping : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Checks if the player is touching any object who's layermask is called Ground
+        // Checks if the player is touching any object whose LayerMask is called Ground
         bool groundCheck = Physics2D.OverlapCapsule(ground.position, new Vector2(1, 0.08f), CapsuleDirection2D.Horizontal, 0, groundMask);
 
+        // Reset jump count when the player is on the ground
+        if (groundCheck)
+        {
+            jumpCount = 0;
+        }
 
-        // Checks if the player can jump
-        // Checks if the player is click the space key, checks if the player is touching ground or water
-        if (Input.GetKeyDown(KeyCode.Space) && (groundCheck || waterCheck ))
+        // Check if the player can jump
+        if (Input.GetKeyDown(KeyCode.Space) && (jumpCount < maxJumps || waterCheck))
         {
             rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, JumpForce);
+            jumpCount++; // Increment jump count each time a jump occurs
         }
     }
 
-    // Checks if the player has touched water, if so let them jump 
+    // Checks if the player has touched water, if so let them jump
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Water")
+        if (collision.gameObject.tag == "Water")
         {
             waterCheck = true;
         }
     }
 
-    // Checks if the player has left water, if so they can't jump 
+    // Checks if the player has left water, if so they can't jump
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Water")
@@ -49,5 +56,4 @@ public class PlayerJumping : MonoBehaviour
             waterCheck = false;
         }
     }
-
 }
